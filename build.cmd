7d9b55f4b83bbba5712a4c850b7d912dec72465b
Thomas Petit <thomas.marc.petit@gmail.com>
2017-02-11 16:31:25 +0100

    make dialog look the same over the whole interface

---
+++ build.cmd
@@ -0,0 +1,3 @@
+@echo off
+del C:\Users\thoma\AppData\Roaming\Kodi\addons\skin.quartz.jarvis-master\*.* /s /q 1>nul
+xcopy C:\Repos\skin.quartz.jarvis C:\Users\thoma\AppData\Roaming\Kodi\addons\skin.quartz.jarvis-master /s /q
