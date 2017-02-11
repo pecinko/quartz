657fe29b9c954e158932b746ad30dd5085006bbc
Thomas Petit <thomas.marc.petit@gmail.com>
2017-02-11 17:11:56 +0100

    cleanup for release

--- build.cmd
+++
@@ -1,3 +0,0 @@
-@echo off
-del C:\Users\thoma\AppData\Roaming\Kodi\addons\skin.quartz.jarvis-master\*.* /s /q 1>nul
-xcopy C:\Repos\skin.quartz.jarvis C:\Users\thoma\AppData\Roaming\Kodi\addons\skin.quartz.jarvis-master /s /q
